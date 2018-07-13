a5708e2028ce50508cfd45622c39ef61c9bac5f9
Johnny Siu <siujohnny@gmail.com>
2018-07-13 16:25:29 -0700

    setting up

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  constructor(){
+    super();
+  }
+
+
+render(){
+  return(
+    <div>
+      HELLO FROM APP
+    </div>
+  )
+}
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = 'Johnny\'s Portfolio';
+import App from './App';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app')
 );
 
